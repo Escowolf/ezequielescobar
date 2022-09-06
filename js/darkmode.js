f6f8ee70a0a2f1e9695185b86b4e5da854048310
Ezequiel Sousa <63311919+Escowolf@users.noreply.github.com>
2022-09-06 16:23:14 -0300

    estilizacao

--- js/darkmode.js
+++ js/darkmode.js
@@ -3,3 +3,10 @@ function darkModeFunction() {
     element.classList.toggle("dark-mode");
  }
 
+ function backToTop(){
+    var btn = document.querySelector("#backToTop");
+    btn.addEventListener("click", function() {
+        window.scrollTo(0, 0);
+    });
+ }
+ 
