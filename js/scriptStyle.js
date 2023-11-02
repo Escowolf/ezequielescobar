1cc072c39bde14e48f707a3414deef39f8c04142
Ezequiel Escobar <escosou@gmail.com>
2023-11-02 08:11:14 -0300

    fix menu mobile

--- js/scriptStyle.js
+++ js/scriptStyle.js
@@ -9,4 +9,13 @@ function backToTop(){
         window.scrollTo(0, 0);
     });
  }
+
+ function showMenu() {
+    var x = document.getElementById("navbar-nav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
  
