b8191e4cc76c1df11bce346a3fda4364b7edc4a8
Ezequiel Sousa <63311919+Escowolf@users.noreply.github.com>
2024-06-11 19:43:09 -0300

    style; add highlight

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <main className="container">
       <section id="introduction" className="introduction">
-        <p className="text-focus-in">I turn any idea into a real project</p>
+        <p className="text-focus-in">I turn any idea into a <span className='highlight'>real project</span></p>
       </section>
     </main>
   );
