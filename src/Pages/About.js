58221b0e049c1954563dc3daffc3e98e6355b4a9
Ezequiel Sousa <63311919+Escowolf@users.noreply.github.com>
2024-06-11 21:23:18 -0300

    style: add perfil image

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './about.css';
 
 function About() {
   return (
     <div className='default-section two-column-section'>
       <p className='left-column'><h1>About</h1>
       My focus of interest has been data persistence, integration via API, requirements gathering, data analysis, integrations, and metadata analysis. I also have a slight flirtation with UX. I am proactive in identifying and resolving problems, always looking for new ways to improve routines and operations.</p>
-      <img className='right-column' src="./images/logo_new.svg" alt="logo Escmeanything" />
+      
+      <div className='right-column'>
+      <img className='perfil' src="./images/perfil.png" alt="null" />
+      <span className='losango1'></span>
+      <span className='losango2'></span>
+      </div>
+      
+
     </div>
   );
 }
